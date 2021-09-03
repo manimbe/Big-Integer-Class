86b259b7ae49d7068fe5bcf4114502203973ff9f
Manimbé <73669682+manimbe@users.noreply.github.com>
2021-09-02 23:19:11 -0300

    Addition function (valueSum)
    Created a function (valueSum) that adds two integers (just the positive part)

--- src/Bignteger_SingleFile.cpp
+++ src/Bignteger_SingleFile.cpp
@@ -103,6 +103,47 @@ public:
 		return 0;
 	}
 	
+	std::string valueSum(Bignteger A, Bignteger B){
+
+		std::string a = A.value;
+		std::string b = B.value;
+		
+		int diff = a.length() - b.length();
+		
+		if(diff > 0){
+			while(diff > 0){
+				b.insert(0,"0");
+				diff--;	
+			}
+		} else if (diff < 0){
+			diff *= -1;
+			while(diff > 0){
+				a.insert(0,"0");
+				diff--;
+			}
+		}
+		
+		char carry = 48;
+		std::string sum = a;
+		
+		for(int i = a.length() -1 ; i>=0 ; i--){
+			sum[i] = (a[i] - 48) + (b[i] - 48) + (carry - 48) + 48;
+			if(sum[i] > 57){
+				sum[i] -= 10;
+				carry = 49;	
+			} else{
+				carry = 48;	
+			}
+		}
+	
+		if(carry == 49){
+			sum.insert(0,"1");
+		}
+	
+		return sum;	
+
+	}
+	
 };
 
 int main(){
